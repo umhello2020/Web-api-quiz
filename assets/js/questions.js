d292fd150a67fed6665af0b9f1311cf68416ca48
Olivia Gilbert <umhello2020@gmail.com>
2022-12-29 16:38:34 -0600

    created quiz questions, choices, and answers

---
+++ assets/js/questions.js
@@ -0,0 +1,28 @@
+// array to include all questions, choices, and answers
+let questions = [
+    //objects to include all components of individual questions
+    {
+        question: 'Names for variables cannot contain which of the following?',
+        choices: ['lowercase letter', 'uppercase letters', 'keywords', 'an underscore'],
+        answer: 'keywords',
+    },
+
+    {
+        question: 'Which of these is a string?',
+        choices: ['1', 'true', '||', '"hi"'],
+        answer: '"hi"',
+    },
+
+    {
+        question: 'Which of these is not a primitive data type?',
+        choices: ['boolean', 'integer', 'string', 'conditional'],
+        answer: 'conditional',
+    },
+
+    {
+        question: 'Where should the script link be at in an HTML page?',
+        chquestion: ['in the header', 'at the bottom of the body', 'in the title', 'in the metadata'],
+        answer: 'at the bottom of the body',
+    },
+   
+];
