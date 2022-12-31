d8d6a628d3b9b7b51cc2998e53d783d87f00a3f7
Olivia Gilbert <umhello2020@gmail.com>
2022-12-31 17:28:16 -0600

    created first two functions

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -2,25 +2,25 @@
 let questions = [
     //objects to include all components of individual questions
     {
-        question: 'Names for variables cannot contain which of the following?',
+        title: 'Names for variables cannot contain which of the following?',
         choices: ['lowercase letter', 'uppercase letters', 'keywords', 'an underscore'],
         answer: 'keywords',
     },
 
     {
-        question: 'Which of these is a string?',
+        title: 'Which of these is a string?',
         choices: ['1', 'true', '||', '"hi"'],
         answer: '"hi"',
     },
 
     {
-        question: 'Which of these is not a primitive data type?',
+        title: 'Which of these is not a primitive data type?',
         choices: ['boolean', 'integer', 'string', 'conditional'],
         answer: 'conditional',
     },
 
     {
-        question: 'Where should the script link be at in an HTML page?',
+        title: 'Where should the script link be at in an HTML page?',
         chquestion: ['in the header', 'at the bottom of the body', 'in the title', 'in the metadata'],
         answer: 'at the bottom of the body',
     },
--- assets/js/script.js
+++ assets/js/script.js
@@ -12,3 +12,54 @@ let startBtnEl = document.getElementById('start');
 let submitBtnEl = document.getElementById('submit');
 let initialsEl = document.getElementById('initials');
 
+//function that begins the quiz
+function startQuiz () {
+    //hides start page
+    let startscreenEl = document.getElementById('start-screen');
+    startscreenEl.setAttribute('class', 'hide');
+
+    //activates question page
+    questionsEl.removeAttribute('class');
+
+    //activate countdown and display it on the screen 
+    countdownId = setInterval(finalTime, 1000);
+    countdownEl.textContent = time;
+
+    // call next function
+    startQuestions();
+}
+
+// function that retrieves questions
+function startQuestions() {
+    // calls question index
+    let liveQuestion = questions[liveQuestionIndex];
+
+    // calls live question's title
+    let titleEl = document.getElementById('title');
+    titleEl.textContent = liveQuestion.title;
+
+    // empty string to clear choices after previous run through
+    choicesEl = '';
+
+    // for loop to cycle through choices with button for each choice
+    for (let i = 0; i < liveQuestion.choices.length; i++) {
+        let choice = liveQuestion.choices[i];
+        let choiceNode = document.createAttribute('button');
+        choiceNode.setAttribute('class', 'choice');
+        choiceNode.setAttribute('value', choice)
+        
+        //puts number in front of the choice option and displays it on screen
+        choiceNode.textContent = i + 1 + '.' + choice;
+        choicesEl.appendChild(choiceNode);
+    };
+        
+}
+
+// left to do
+// 1. create function to manage clicking on the choice and affecting the timer and calling the feedback
+// 2. function to end the quiz
+// 3. function finalTime to update time and check if user ran out of time
+// 4. function to save the highscore to storage
+// 5. function to save score if user presses enter instead of clicking the button
+// 6. create event listeners for submit and start buttons(click)
+// 7. create event listeners for choices(click) and initials(enter key)
